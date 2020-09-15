0c01efaf2825841d568788d7d12b6c0b1cd1a50a
Alexander Sutterfield <Alex@vekroe.com>
2020-09-15 02:08:25 -0400

    Comments added

--- app.js
+++ app.js
@@ -13,18 +13,20 @@ Load files
 ***************************
 */
 
+//creates an array containing names of all the files in the inputs folder
 var files = fs.readdirSync('./Input/');
 
-for (x of files) {
-    console.log(x.slice(-3))
-    if (x.slice(-3) == '.js') {
-        jsonInput(x);
+for (x of files) { //cycle through all files
+   
+    //sorts csv from js
+    if (x.slice(-3) == '.js') { //checks if file is json
+        jsonInput(x); //turns json to csv
     }
-    else if (x.slice(-3) == 'csv') {
-        csvInput(x);
+    else if (x.slice(-3) == 'csv') { //checks if file is csv
+        csvInput(x); //turns csv to json
     }
-    else {
-        console.log(`${x} is not a valid file type`)
+    else { //if unknown file type 
+        console.log(`${x} is not a valid file type`);
     }
 }
 
@@ -35,21 +37,24 @@ Json -> CSV
 */
 
 function jsonInput(input) {
-    //Import file to variable
-    //If I have time I will auto detect names of a file
-    var convertFrom = './Input/' + input;
-    var convertTo =  './Output/' + input.slice(0,-3) + '.csv';
-    var file = require(convertFrom); //this is a nested JSON array so the array we actually need is stored in todos[questions]
-    console.log(file['questions']);
 
-    // convert JSON array to CSV string
+    //Import file to variable
+    var convertFrom = './Input/' + input; //sets the location of the file
+    var convertTo =  './Output/' + input.slice(0,-3) + '.csv'; //sets the location to save to
+
+    //gets the file contents
+    var file = require(convertFrom); //this is a nested JSON array so the array we actually need is stored in file[questions]
+
+    // convert JSON array to CSV
     JSONtoCSV.json2csv(file['questions'], (err, csv) => { //converts file[questions] to csv format
+        
         if (err) { //error handling 
             throw err; //error to console
         }
 
         // write CSV to a file
-        fs.writeFileSync(convertTo, csv); //If I have time I will get this to auto name
+        fs.writeFileSync(convertTo, csv);
+
     });
 
 }
@@ -59,25 +64,26 @@ function jsonInput(input) {
 CSV -> JSON
 ***************************
 */
-function csvInput(input) {
-    var convertFrom = './Input/' + input;
-    var convertTo =  './Output/' + input.slice(0,-4) + '.js';
-    
-    CSVToJSON().fromFile(convertFrom)
-    .then(file => {
 
-        // users is a JSON array
-        // log the JSON array
-        console.log(file);
+function csvInput(input) {
+
+    //Import file to variable
+    var convertFrom = './Input/' + input; //sets the location of the file
+    var convertTo =  './Output/' + input.slice(0,-4) + '.js'; //sets the location to save to
+    
+    //convert CSV to JSON array
+    CSVToJSON().fromFile(convertFrom) 
+    .then(file => { //file is the new JSON Array
         
+        //write JSON to a js file
         fs.writeFile(convertTo, JSON.stringify(file, null, 4), (err) => {
-            if (err) {
-                throw err;
+            
+            if (err) { //error handling
+                throw err; //error handling
             }
-            console.log(`JSON array ${input.slice(0, -4)} is saved.`);
         });
-    }).catch(err => {
-        // log error if any
-        console.log(err);
+
+    }).catch(err => { //error handling
+        console.log(err); //error handling
     });
 }
