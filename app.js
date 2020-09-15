267fb4f249eca26ab09aa6556ad118eaa4464073
Alexander Sutterfield <Alex@vekroe.com>
2020-09-15 01:29:28 -0400

    Still need to comment

--- app.js
+++ app.js
@@ -3,8 +3,30 @@
 Import needed Libraries
 ***************************
 */
-const converter = require('json-2-csv'); //for json to csv
+const JSONtoCSV = require('json-2-csv'); //for json to csv
+const CSVToJSON  = require('csvtojson'); //for csv to json
 const fs = require('fs'); //for file writing 
+
+/*
+***************************
+Load files
+***************************
+*/
+
+var files = fs.readdirSync('./Input/');
+
+for (x of files) {
+    console.log(x.slice(-3))
+    if (x.slice(-3) == '.js') {
+        jsonInput(x);
+    }
+    else if (x.slice(-3) == 'csv') {
+        csvInput(x);
+    }
+    else {
+        console.log(`${x} is not a valid file type`)
+    }
+}
 
 /*
 ***************************
@@ -12,22 +34,49 @@ Json -> CSV
 ***************************
 */
 
-//Import file to variable
-//If I have time I will auto detect names of a file
-var todos = require('./todos.js'); //this is a nested JSON array so the array we actually need is stored in todos[questions]
+function jsonInput(input) {
+    //Import file to variable
+    //If I have time I will auto detect names of a file
+    var convertFrom = './Input/' + input;
+    var convertTo =  './Output/' + input;
+    var file = require(convertFrom); //this is a nested JSON array so the array we actually need is stored in todos[questions]
 
-// convert JSON array to CSV string
-converter.json2csv(todos['questions'], (err, csv) => { //converts todos[questions] to csv format
-    if (err) { //error handling 
-        throw err; //error to console
-    }
+    // convert JSON array to CSV string
+    JSONtoCSV.json2csv(file['questions'], (err, csv) => { //converts file[questions] to csv format
+        if (err) { //error handling 
+            throw err; //error to console
+        }
 
-    // write CSV to a file
-    fs.writeFileSync('todos.csv', csv); //If I have time I will get this to auto name
-});
+        // write CSV to a file
+        fs.writeFileSync(convertTo, csv); //If I have time I will get this to auto name
+    });
+
+}
 
 /*
 ***************************
 CSV -> JSON
 ***************************
 */
+function csvInput(input) {
+    var convertFrom = './Input/' + input;
+    var convertTo =  './Output/' + input;
+    
+    CSVToJSON().fromFile(convertFrom)
+    .then(file => {
+
+        // users is a JSON array
+        // log the JSON array
+        console.log(file);
+        
+        fs.writeFile(convertTo, JSON.stringify(file, null, 4), (err) => {
+            if (err) {
+                throw err;
+            }
+            console.log(`JSON array ${input.slice(0, -4)} is saved.`);
+        });
+    }).catch(err => {
+        // log error if any
+        console.log(err);
+    });
+}
